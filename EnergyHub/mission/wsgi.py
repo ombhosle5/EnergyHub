71c0aebcc1363c50fb1e831600e1e59b6ff2378f
Om Bhosle <137046376+ombhosle5@users.noreply.github.com>
2026-09-24 12:56:54 +0530

    Modify WSGI config to include 'app' variable for vercel
    Added 'app' variable for Vercel deployment.

--- EnergyHub/mission/wsgi.py
+++ EnergyHub/mission/wsgi.py
@@ -2,9 +2,6 @@
 WSGI config for mission project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
@@ -14,3 +11,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mission.settings')
 
 application = get_wsgi_application()
+
+# Vercel looks for a variable named "app"
+app = application
